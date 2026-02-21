d4a98fae8aa031d4c7803c6187b05fa075dff11d
Webwiz <aliuanas004@gmail.com>
2026-02-21 16:00:30 +0100

    golang todo rest api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,20 @@
 package cmd
 
-import "fmt"
+import (
+	// "fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	var router
+	var router *gin.Engine = gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Todo App is running",
+			"status":  "success",
+		})
+	})
+
+	router.Run(":3000")
 }
