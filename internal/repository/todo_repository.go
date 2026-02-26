49960b8226687f11496c76365142044c7a2e42e4
Webwiz <aliuanas004@gmail.com>
2026-02-26 22:25:15 +0100

    creating the repository...

---
+++ internal/repository/todo_repository.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"context"
+	"time"
+	"todo_api/internal/models"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CreateTodo(pool *pgxpool.Pool, title string, completed bool)(*models.Todo, error) {
+	// var ctx context.Context
+	// var cancel context.CancelFunc
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	query := `
+				INSERT INTO todos_user (title, completed) 
+				VALUES ($1, $2) 
+				RETURNING id, title, completed, created_at, updated_at
+	`	
+
+	var todo models.Todo
+
+	err := pool.QueryRow(ctx, query, title, completed).Scan(
+		&todo.ID, 
+		&todo.Title, 
+		&todo.Completed, 
+		&todo.CreatedAt, 
+		&todo.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &todo, nil
+}
